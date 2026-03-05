47a86e209148e1f10f418b4cc8702013b0c78d81
Samuel Mahembe <samuelmahembe3@gmail.com>
2026-03-04 22:52:33 -0500

    logging

--- src/logger.py
+++ src/logger.py
@@ -15,11 +15,4 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s')
-
-if __name__ == "__main__":
-    try:
-        1/0
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-        raise CustomException(e,sys)
     
